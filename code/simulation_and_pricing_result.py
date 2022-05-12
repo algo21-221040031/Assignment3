c2078542ebf78de95f394bcd758cb98f9b905987
algo21-221040031 <101002984+algo21-221040031@users.noreply.github.com>
2022-05-12 11:29:25 +0800

    Pricing and simulation result of option pricing.

--- code/simulation_and_pricing_result.py
+++ code/simulation_and_pricing_result.py
@@ -1,8 +1,7 @@
 
-from DerivativesProject import *
+from derivative_pricing_models import *
 
 '''
-以
 S = 166.960
 r = 0.00765
 q = 0.027007
@@ -10,21 +9,21 @@ T1 = 0.1
 T2 = 0.3
 K1 = 10
 K2 = 100
-为例进行蒙特卡洛模拟、最小二乘蒙特卡洛模拟以及三叉树定价
+Monte Carlo Simulation, Binomial Tree Pricing, Trinomial Tree Pricing and Least Squares Monte Carlo Simulation.
 '''
 T1 = 0.1
 T2 = 0.3
 K1 = 10
 K2 = 100
 
-print("欧式Y的二叉树定价：")
+print("Binomial tree of european compound option:")
 print(e_Y_price(S,K1,K2,T1,T2,r,q,100,sigma))
-print("Y的二叉树定价：")
+print("Binomial tree of american compound option:")
 print(Y_price(S,K1,K2,T1,T2,r,q,100,sigma))
-print('Y的三叉树定价：')
+print('Trinomial tree of american compound option:')
 print(C_TT_Am_Y(S, K1, K2, T1, T2, r ,q, 100, sigma))
-print('蒙特卡洛模拟模拟欧式Y的价格：')
-print(Y_price_simulations(S,r,q,T1,T2,sigma,K1,K2,1000))#重复1000次实验
-print('最小二乘蒙特卡洛模拟模拟Y的价格：')
-p =Y_MC_American_call_price(r,q,sigma,S,T1,T2,100,1000,K1,K2)#1000路径，100步
+print('Monte Carlo Simulation of european compound option:')
+print(Y_price_simulations(S,r,q,T1,T2,sigma,K1,K2,1000))
+print('Least Squares Monte Carlo Simulation of american compound option:')
+p =Y_MC_American_call_price(r,q,sigma,S,T1,T2,100,1000,K1,K2)
 print(p)
