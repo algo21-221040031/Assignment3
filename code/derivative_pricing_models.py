a48b6fe27e1ac7e8bc54c55db151a6d853cb3f17
algo21-221040031 <101002984+algo21-221040031@users.noreply.github.com>
2022-05-12 11:02:41 +0800

    Update derivative_pricing_models.py
    Modify the details of least squares monte carlo simulation.

--- code/derivative_pricing_models.py
+++ code/derivative_pricing_models.py
@@ -1,4 +1,4 @@
-
+from __future__ import division
 import numpy as np
 from scipy.stats import norm
 from random import gauss
@@ -6,18 +6,19 @@ import math
 from math import exp, sqrt,log
 import matplotlib.pyplot as plt
 import pandas as pd
-from __future__ import division
+
 '''
-S:当前股价
-K:执行价
-T:到期时间
-r:无风险利率
-q:红利率
-sigma:波动率
-N:二叉树步数
-dt:单位时间间隔
+S: Current stock price
+K: Strike Price
+T: Expiration Date
+r: risk-free rate
+q: dividend yield
+sigma: volatility
+N: Step numbers of binomial tree
+dt: Time gap
 '''
-#欧式看涨期权的BS定价用于验证二叉树定价的准确性
+
+# B-S formula to test the accuracy of binomial tree model
 def c_BS(S,K,T,r,q,sigma):
     sigma_sqrt_t = sigma*(T**0.5)
     df = r-q+0.5*sigma**2
@@ -27,7 +28,8 @@ def c_BS(S,K,T,r,q,sigma):
     nd2 = norm.cdf(df2)
     call_price = S*math.exp(-q*T)*nd1 -K*math.exp(-r*T)*nd2
     return call_price
-#欧式看涨期权的二叉树定价  
+
+# Binomial tree pricing model on european call option
 def e_bina_call(S,K,T,r,q,N,sigma):
     dt = T/N
     #u = math.exp( sigma*(dt**0.5))
@@ -42,7 +44,8 @@ def e_bina_call(S,K,T,r,q,N,sigma):
         for j in range(0,i+1):
             lat[i,j] = math.exp(-r*dt)*(p* lat[i+1,j+1] + (1-p) * lat[i+1,j])
     return lat[0,0]
-#美式看涨期权的二叉树定价
+
+# Binomial tree pricing model on amrican call option
 def a_bina_call(S,K,T,r,q,N,sigma):
     dt = T/N
     u = math.exp((r-q)*dt + sigma*(dt**0.5))
@@ -58,7 +61,7 @@ def a_bina_call(S,K,T,r,q,N,sigma):
             lat[i,j] = max(S*(u**j)*(d**(i-j))-K,math.exp(-r*dt)*(p* lat[i+1,j+1] + (1-p) * lat[i+1,j]))
     return lat[0,0]
 
-#Y期权定价
+# Binomial tree pticing model on compound option
 def Y_price(S,K1,K2,T1,T2,r,q,N,sigma):
     dt = T1/N
     u = math.exp((r-q)*dt + sigma*(dt**0.5))
@@ -79,7 +82,7 @@ def Y_price(S,K1,K2,T1,T2,r,q,N,sigma):
             '''
     return lat[0,0]
 
-#若Y为欧式期权的定价    
+# Pricing model when compound option is european   
 def e_Y_price(S,K1,K2,T1,T2,r,q,N,sigma):
     dt = T1/N
     u = math.exp((r-q)*dt + sigma*(dt**0.5))
@@ -94,7 +97,7 @@ def e_Y_price(S,K1,K2,T1,T2,r,q,N,sigma):
             lat[i,j] = math.exp(-r*dt)*(p* lat[i+1,j+1] + (1-p) * lat[i+1,j])
     return lat[0,0]
 
-#关于复合期权的三叉树定价
+# Trinomial tree pricing model of compound option
 def C_TT_Am_Y(S, K1, K2, T1, T2, r ,q, N, sigma):
     
     dt = (T1) / N 
@@ -111,7 +114,7 @@ def C_TT_Am_Y(S, K1, K2, T1, T2, r ,q, N, sigma):
     stock = np.zeros((2 * N + 1, N + 1))
     for i in range(0, N+1):
         for j in range(0, 2 * i + 1):
-            #每一个node的股价：
+            # stock price at each node：
             if j <= i:
                 stock[j,i] = S * math.pow(u, i - j) * math.pow(d, 0)
             else:
@@ -121,7 +124,7 @@ def C_TT_Am_Y(S, K1, K2, T1, T2, r ,q, N, sigma):
     X = np.zeros((2 * N + 1, N + 1))
     for i in range(0, N+1):
         for j in range (0, 2 * i + 1):
-        #使用hi时刻的股价分布情况计算X的期权价格，使用BS
+        # calculate the price of X at time hi, using BS formula
             X[j,i] = c_BS(stock[j,i], K2, T2,r,  q, sigma)
             #c_BS(S,K,T,r,q,sigma):
      
@@ -141,17 +144,18 @@ def C_TT_Am_Y(S, K1, K2, T1, T2, r ,q, N, sigma):
                 + p_d * C_Y_Am[2,1]))
     
     return C_Y_Am[0,0]
-#蒙特卡洛模拟欧式
+
+# Monte-Carlo european option pricing simulation
 def calculate_S_T(S, sigma, q,r, T):
-    #模拟几何布朗运动，算出最后的S_T
+    # simulate the geometric brownian motion
     return S * exp((r -q- 0.5 * sigma ** 2) * T + sigma * sqrt(T) * gauss(0.0, 1.0))
 
 def option_payoff(S_T, K):
-    #定义欧式期权收益
+    # Define the payoff of european option
     return max(S_T - K, 0.0)
       
 def option_price(S, q,r, T, sigma, K, simulations):
-    #模拟欧式模拟期权收益
+    # simulate the payoff of the european option
     payoffs = []
     discout = exp(-r * T)
     for i in range(simulations):
@@ -161,8 +165,9 @@ def option_price(S, q,r, T, sigma, K, simulations):
         )
     price = discout * sum(payoffs) / float(simulations)
     return price
+
 def Y_price_simulations(S,r,q,T1,T2,sigma,K1,K2,simulations):
-    #模拟欧式Y期权的收益
+    # simulate the payoff of the compound option
     payoffs = []
     discout = exp(-r * T1)
     for i in range(simulations):
@@ -174,16 +179,17 @@ def Y_price_simulations(S,r,q,T1,T2,sigma,K1,K2,simulations):
         )
     y_price = discout * sum(payoffs) / float(simulations)
     return y_price
-#最小二乘蒙特卡洛模拟美式Y
+
+# Least-Square Monte-Carlo to simulate the price of compound option
 def sample_paths(r, q,sigma, S_0, T1,T2 ,M, N,K2):
     dt = T1/M
     '''
-    r: 无风险利率
-    sigma: 股价波动率
-    S_0: 初始股价
-    T: 期权期限
-    M: 股价变化过程离散化的段数
-    N: 路径数
+    r: risk-free rate
+    sigma: volatility
+    S_0: initial stock price
+    T: option period
+    M: number of discretization
+    N: number of paths
     '''
     data = S_0*np.ones((N,1))
     for i in range(M):
@@ -194,7 +200,8 @@ def sample_paths(r, q,sigma, S_0, T1,T2 ,M, N,K2):
         for j in range(M+1):
             data[i,j] = c_BS(data[i,j],K2,T2,r,q,sigma)
     return data
-#最小二乘法拟合每个时间节点
+
+# Least Squares to simulate each time node
 def linear_fitting(X,Y):
     X = np.array(X)
     Y = np.array(Y)
@@ -206,7 +213,8 @@ def linear_fitting(X,Y):
     fitted_coeff = np.matmul(inv_coeff_mat,target_vec)
     resulted_Ys = fitted_coeff[0]+fitted_coeff[1]*X+fitted_coeff[2]*X*X
     return resulted_Ys
-#模拟出Y的价格
+
+# simulate the price of Y
 def Y_MC_American_call_price(r, q,sigma, S_0, T1,T2 ,M, N,K1,K2):
     data = sample_paths(r, q,sigma, S_0, T1,T2 ,M, N,K2)
     option_prices = np.maximum(data[:,-1]-K1, 0)
@@ -217,56 +225,55 @@ def Y_MC_American_call_price(r, q,sigma, S_0, T1,T2 ,M, N,K1,K2):
     option_prices *= np.exp(-r*T1/M)
     return np.average(option_prices)
 
-#希腊值计算
-#delta计算：股价上升0.01，Y价格的变动
+# Calculation of greeks
+# Calcultion of delta
 def delta(S0,K1,K2,T1,T2,r,q,N,sigma):
     S1 = S0+0.01
-    '''欧式'''
+    '''European'''
     e0 = e_Y_price(S0,K1,K2,T1,T2,r,q,N,sigma)
     e1 = e_Y_price(S1,K1,K2,T1,T2,r,q,N,sigma)
     e_delta = (e1-e0)/0.01
-    '''美式'''
+    '''American'''
     a0 = Y_price(S0,K1,K2,T1,T2,r,q,N,sigma)
     a1 = Y_price(S1,K1,K2,T1,T2,r,q,N,sigma)
     a_delta = (a1-a0)/0.01
     return (e_delta,a_delta)
 
-#gamma计算:
+# Calculation of gamma
 def gamma(S0,K1,K2,T1,T2,r,q,N,sigma):
     S1 = S0+0.01
-    '''欧式'''
+    '''European'''
     e_delta0 = delta(S0,K1,K2,T1,T2,r,q,N,sigma)[0]
     e_delta1 = delta(S1,K1,K2,T1,T2,r,q,N,sigma)[0]
     e_gamma = (e_delta1-e_delta0)/0.01
-    '''美式'''
+    '''American'''
     a_delta0 = delta(S0,K1,K2,T1,T2,r,q,N,sigma)[1]
     a_delta1 = delta(S1,K1,K2,T1,T2,r,q,N,sigma)[1]
     a_gamma = a_delta1/0.01 - (a_delta0/0.01)
     return (e_gamma,a_gamma)
 
-#theta值的计算
+# Calculation of theta
 def theta(S,K1,K2,T1,T2,r,q,N,sigma):
     n_T= T1 -1/365
-    '''欧式'''
+    '''European'''
     e0 = e_Y_price(S,K1,K2,T1,T2,r,q,N,sigma)
     e1 = e_Y_price(S,K1,K2,n_T,T2,r,q,N,sigma)
     e_theta = (e1-e0)*365
-    '''美式'''
+    '''American'''
     a0 = Y_price(S,K1,K2,T1,T2,r,q,N,sigma)
     a1 = Y_price(S,K1,K2,n_T,T2,r,q,N,sigma)
     a_theta = (a1-a0)*365
     return (e_theta,a_theta)
 
-#隐含波动率的迭代计算
+# Binary search of implied volatility
 def bsm_imp_vol(S,K,T,r,c,q):
-    c_est = 0 # 期权价格估计值
-    top = 1  #波动率上限
-    floor = 0  #波动率下限
-    sigma = ( floor + top )/2 #波动率初始值
-    count = 0 # 计数器
+    c_est = 0 
+    top = 1  
+    floor = 0 
+    sigma = ( floor + top )/2 
+    count = 0 
     while abs( c - c_est ) > 0.000001:
         c_est = c_BS(S,K,T,r,q,sigma)
-        #根据价格判断波动率是被低估还是高估，并对波动率做修正
         count += 1       
         if count > 100:
             sigma = 0
@@ -279,7 +286,7 @@ def bsm_imp_vol(S,K,T,r,c,q):
             sigma = ( sigma + floor )/2
     return sigma  
 
-#隐含波动率迭代计算:牛顿法
+# Newton-method to calculate the implied volatility
 
 def Vega(S,K,T,r,q,sigma):
     d1 = (math.log(S / K) + (r - q + 1 / 2 * pow(sigma, 2)) * T) / (sigma * math.sqrt(T))
