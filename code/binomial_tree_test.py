703984f7f60d9744e16682f5cf82d46dc1c4b92a
algo21-221040031 <101002984+algo21-221040031@users.noreply.github.com>
2022-05-12 11:07:53 +0800

    Plot the result of tree models and BS model.

--- code/binomial_tree_test.py
+++ code/binomial_tree_test.py
@@ -13,6 +13,7 @@ r = 0.00765
 q = 0.027007
 K = 180
 T = 0.3835616438356
+
 #N = 100
 sigma = 0.3007
 n=np.array(range(1,200))
@@ -22,6 +23,7 @@ e = [e_bina_call(S, K, T, r, q,i,sigma) for i in n]
 plt.figure(figsize=(9, 5))
 plt.plot(n, e, label='CRR')
 plt.axhline(BSM_benchmark, color='r', ls='dashed', lw=1.5,label='BSM')
+
 #plt.axhline(BSM_benchmark, color='r', ls='dashed', lw=1.5,label='BSM')
 plt.xlabel('steps')
 plt.ylabel('europe call premium')
